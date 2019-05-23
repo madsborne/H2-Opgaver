91af3ac7b522ed3ca8ece23d7274c7349f734b48
Mads Borne Sparre <mads648i@zbc.dk>
2019-05-23 21:30:46 +0200

    assignment 3 done

--- ThreadSyncOpg1/ThreadSyncOpg1/Program.cs
+++ ThreadSyncOpg1/ThreadSyncOpg1/Program.cs
@@ -9,40 +9,68 @@ namespace ThreadSyncOpg1
 {
     class Program
     {
-        static int adding = 0;
+        // my statics the will be used to count up and to debug 
+        public static int counter = 0;
+        public static int doubleCounter = 0;
         static void Main(string[] args)
         {
-            CounterOne one = new CounterOne();
-            one.CounterNumberOne();
+            // Creates two threads that runs the two call back methods
+            Thread threadOne = new Thread(CounterOne.CounterNumberOne);
+            threadOne.Start();
+            Thread threadTwo = new Thread(Remover.CounterRemoveOne);
+            threadTwo.Start();
+
+            threadTwo.Join();
+            threadOne.Join();
+
             Console.ReadLine();
         }
+
+        
     }
 
     class CounterOne
     {
-        
-        public void CounterNumberOne()
+        // creats my lock object 
+        public static object gridLock = new object();
+
+        public static void CounterNumberOne(object arg)
         {
-            Thread threadOne = new Thread(CounterNumberOne);
-            threadOne.Start();
             while (true)
             {
-                adding = +2;
-                Console.WriteLine(adding);
+
+                // these are for the assignment 1 and 2 
+                //Program.counter += 2;
+                //Console.WriteLine(Program.counter);
+
+                // locking off the double counter to make sure it has the right to it and then sleeps for two seconds
+                lock (gridLock)
+                {
+                    Program.doubleCounter += 60;
+                    Console.WriteLine("************************************************************ " + Program.doubleCounter);
+                }
                 Thread.Sleep(2000);
             }
         }
+    }
 
-        public void CounterRemoveOne()
+    // the name makes sense for assignment 1 but i combined it for the two others
+    class Remover
+    {
+        public static void CounterRemoveOne(object arg)
         {
-           ThreadPool.qu
             while (true)
             {
-                adding = -1;
-                Console.WriteLine(adding);
-                threadTwo.
+                // these are for the assignment 1 and 2
+                //Program.counter -= 1;
+                //Console.WriteLine(Program.counter);
+
+                // makes the thread sleep for 1mill sec to then run the rest just to make sure it comes in second 
+                Thread.Sleep(100);
+                Program.doubleCounter += 60;
+                Console.WriteLine("############################################################ " + Program.doubleCounter);
+                Thread.Sleep(2000);
             }
         }
-
     }
 }
