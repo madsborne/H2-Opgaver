658032dfcc860a4cb13e4199010cda2bea9dc1ac
Mads Borne Sparre <mads648i@zbc.dk>
2019-06-05 14:50:41 +0200

    Soild1

---
+++ SOLIDOpg1/SOLIDOpg1/Trapeze.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDOpg1
+{
+    class Trapeze : Square
+    {
+        public Trapeze(int numberOne, int numberTwo) : base(numberOne,numberTwo)
+        {
+
+        }
+    }
+}
---
+++ SOLIDOpg1/SOLIDOpg1/square.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDOpg1
+{
+    class Square
+    {
+        private int side;
+        public int Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                side = value;
+            }
+        }
+        
+        public Square(int areaNumber, int steepNumber)
+        {
+            Side = areaNumber;
+        }
+
+        public double Circumference()
+        {
+            int circumNumber = side * 4;
+
+            return circumNumber;
+        }
+
+        /// <summary>
+        /// takes the side number that we get from the constructer and * it 4 times and then returns the number
+        /// </summary>
+        /// <returns></returns>
+        public virtual int Area()
+        {
+            int areaNumber = side * side;
+
+            return areaNumber;
+        }
+    }
+}
