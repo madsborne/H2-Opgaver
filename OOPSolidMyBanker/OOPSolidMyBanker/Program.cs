339546eb659bd4e9523571eab88ab992caf12b1d
Mads Borne Sparre <mads648i@zbc.dk>
2019-06-05 14:48:08 +0200

    mybanker

---
+++ OOPSolidMyBanker/OOPSolidMyBanker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSolidMyBanker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
